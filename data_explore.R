57985ba29d3bf8b92bf277f62ae89b951b16c062
valerie bataille <bataille.valerie@epa.gov>
2017-05-10 09:45:07 -0400

    added data file

--- data_explore.R
+++ data_explore.R
@@ -1,9 +1,12 @@
 library(dplyr)
 
-dat2015 <- read.csv("~/projects/cyanoviz/data_clean_2015.csv")
+dat2015 <- read.csv("S:/projects/cyanoviz/data_clean_2015.csv", stringsAsFactors = F)
 
+copy_dat <- dat2015
 head(waterbody.id)
 head(dat)
+
+
 
 
 
