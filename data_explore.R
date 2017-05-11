5aeed861082a9b361918f6e9a2d7dc3b2a49dece
valerie bataille <bataille.valerie@epa.gov>
2017-05-11 13:03:20 -0400

    scatter plots of sample dates of phyco and chla

--- data_explore.R
+++ data_explore.R
@@ -1,12 +1,16 @@
 library(dplyr)
 
-dat2015 <- read.csv("S:/projects/cyanoviz/data_clean_2015.csv", stringsAsFactors = F)
+dat2015 <- read.csv("data_clean_2015.csv", stringsAsFactors = FALSE)
 
-copy_dat <- dat2015
-head(waterbody.id)
-head(dat)
+tbl_df(dat2015)
+count_2015 <- dat2015 %>% 
+  group_by(waterbody_id) %>% 
+  count()
+count_2015
+
+#Try summarize()
+ggplot(data=dat2015)+geom_point(mapping=aes(x=sample_date,y=chla_ugl))
+ggplot(data=dat2015)+geom_point(mapping=aes(x=sample_date,y=phyco_ugl))
 
 
-
-
-
+ggplot(data=dat2015,mapping=aes(x=sample_date,y=Chla_ugl))+geomp_point(mapping=aes(color=waterbody_id))+geom_smooth()
