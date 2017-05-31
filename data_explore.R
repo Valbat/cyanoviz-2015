7ccf89ba7b052f6518aebaeb44441bc0c7d7f893
valerie bataille <bataille.valerie@epa.gov>
2017-05-31 11:26:35 -0400

    sample temp

--- data_explore.R
+++ data_explore.R
@@ -19,7 +19,7 @@ count_2015
 #scatter plots in ggplot
 chla_gg <- ggplot(data=dat2015, mapping = aes(x=sample_date,y=chla_ugl)) + 
   geom_point() +
-  facet_wrap(~ state)
+  facet_wrap(~ state) +
   geom_smooth(method = "lm")
 chla_gg
 
@@ -30,7 +30,7 @@ ggplot(dat2015,aes(station_longitude,station_latitude, color = state)) +
   geom_point()
 
 
-ggplot(data=dat2015,mapping=aes(x=sample_date,y=Chla_ug))+geomp_point(mapping=aes(color=waterbody_id))+geom_smooth()
+ggplot(data=dat2015,mapping=aes(x=sample_date,y=Chla_ug))+geom_point(mapping=aes(color=waterbody_id))+geom_smooth()
 
 R version 3.2.2 (2015-08-14) -- "Fire Safety"
 Copyright (C) 2015 The R Foundation for Statistical Computing
@@ -132,7 +132,7 @@ boxplot(log1p(nonegphyco$phyco_ugl)~nonegphyco$state)
 
 plot(log1p(nonegphyco$PTL),log1p(nonegphyco$phyco_ugl)
      
-summary(dat2015$sample_temp_c)
+     summary(dat2015$sample_temp_c)
 boxplot(log1p(dat2015$chla_ugl)~dat2015$chla_ugl$state)
 boxplot(dat2015$chla_ugl)
 boxplot(log1p(dat2015$chla_ugl))
@@ -317,7 +317,90 @@ plot(density(log1p(warwickpond$phyco_ugl)))
 arrange(desc(chla_ugl))
 highphyco <- select(waterbody_id="RI0007024L-02",chla_ugl,sample_date)
 
-ggplot(data=warwickpond)+
-geom_bar(mappping=aes(x=sample_date,y=phyco_ugl))  
+ggplot(data=warwickpond, mappping=aes(x=sample_date,y=phyco_ugl))+
+geom_bar()  
 plot(log1p(warwickpond$phyco_ugl))
 plot(log1p(warwickpond[,3:4]))
+
+dat2015 <- read_csv("may data_clean_2015.csv",guess_max = 2000) %>%
+  filter(complete.cases(data.frame(chla_ugl,phyco_ugl))) %>%
+  filter(chla_ugl > 0) %>% 
+  filter(phyco_ugl > 0.1) %>%
+  arrange(desc(chla_ugl), desc(phyco_ugl)) %>%
+  filter(dilution == "1:1") %>%
+  filter(analysis_rep == "Primary") %>%
+  filter(fluorometer_type == "Beagle") %>%
+  filter(sample_temp_c > 20 & sample_temp_c < 24)
+
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+ggplot(data=dat2015, mapping=aes(x=sample_date,y=chla_ugl))+
+  geom_point() +
+  geom_smooth()
+
+ggplot(data=warwickpond)+
+  geom_plot(mapping=aes(x=sample_date,y=chla_ugl))
+geom
+
+dat2015 <- read_csv("may data_clean_2015.csv",guess_max = 2000) %>%
+  filter(complete.cases(data.frame(chla_ugl,phyco_ugl)))%>%
+  filter(chla_ugl > 0) %>% 
+  filter(phyco_ugl > 0.1) %>%
+  filter(dilution == "1:1") %>%
+  filter(analysis_rep == "Primary") %>%
+  filter(fluorometer_type == "Beagle") %>%
+  filter(sample_temp_c > 20 & sample_temp_c < 24)
+ 
+warwickpond <- filter(dat2015,waterbody_id=="RI0007024L-02") %>%
+  select(state,waterbody_name,chla_ugl,phyco_ugl,sample_date)
+
+plot(warwickpond$phyco_ugl,warwickpond$chla_ugl)
+hist(warwickpond$phyco_ugl)
+
+plot(warwickpond$sample_date,warwickpond$phyco_ugl,main="Cyano monitoring 2015 Warwick Pond")
+
+plot(warwickpond$sample_date,warwickpond$phyco_ugl,main="Cyano monitoring 2015 Warwick Pond",xlabel="sample Date",ylabel="Phycocyanin")                                                 
+#error with trying to run this?? 
+
+plot(warwickpond$sample_date,warwickpond$chla_ugl,main="Cyano Monitoing 2015 Warwick Pond",xlabel="Sample Date",ylabel="Chlorophyll")
+
+
+
+install.packages("randomForest")
+
+library("ggplot2")
+library("dplyr")
+     
+library(ggplot2)
+library(dplyr)
+
+dat2015 <- read_csv("may data_clean_2015.csv",guess_max = 2000) %>%
+  filter(complete.cases(data.frame(chla_ugl,phyco_ugl)))%>%
+  filter(chla_ugl > 0) %>% 
+  filter(phyco_ugl > 0.1) %>%
+  filter(dilution == "1:1") %>%
+  filter(analysis_rep == "Primary") %>%
+  filter(fluorometer_type == "Beagle")
+  
+plot(dat2015$chla_ugl,dat2015$water_temp_c,main="Samples collected within QAPP temperature range")
+  abline(h=20)%>%
+  abline(h=24)
+
+
+plot(dat2015$phyco_ugl,dat2015$water_temp_c,main="Samples collected within QAPP temperature range")
+  abline(h=20)%>%
+  abline(h=24)
+
+summary(dat2015$sample_temp_c >24)
+# Mode       FALSE    TRUE    NA's (510 observations)
+# logical     271      67     172 
+summary(dat2015$sample_temp_c <20)
+#Mode         FALSE    TRUE    NA's 
+#logical       24     314     172 
+
+stem(dat2015$water_temp_c)
+table(dat2015$water_temp_c)
+
+temp_chla_gg <- ggplot(data = dat2015, mapping = aes(x=water_temp_c, y=chla_ugl)) + 
+  geom_point()
