f64f3af9611844c114e1a7af83f28ab9c7e3959d
valerie bataille <naturebat79@gmail.com>
2017-06-30 10:58:12 -0500

    let's see

--- Scripts/clean data script.R
+++ Scripts/clean data script.R
@@ -1,3 +1,7 @@
+library(tidyverse)
+library(dplyr)
+
+
 dat2015 <- read_csv("may data_clean_2015.csv",guess_max = 2000) %>%
   filter(complete.cases(data.frame(chla_ugl,phyco_ugl)))%>%
   filter(chla_ugl > 0) %>% 
@@ -6,3 +10,4 @@ dat2015 <- read_csv("may data_clean_2015.csv",guess_max = 2000) %>%
   filter(analysis_rep == "Primary") %>%
   filter(fluorometer_type == "Beagle") %>%
   filter(sample_temp_c > 20 & sample_temp_c < 24)
+
